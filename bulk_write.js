4402aa996d53c4fa0f1ee378eb283745722e4678
dndinidn <andiniputriamrullah@gmail.com>
2025-06-19 20:23:34 +0800

    Bulk Write

---
+++ bulk_write.js
@@ -0,0 +1,14 @@
+db.produk.insertMany([
+  {
+    nama: "Kulkas 2 Pintu",
+    kategori: "Kulkas",
+    harga: 3200000,
+    stok: 5,
+  },
+  {
+    nama: "Mesin Cuci Otomatis",
+    kategori: "Mesin Cuci",
+    harga: 2700000,
+    stok: 7,
+  },
+]);
