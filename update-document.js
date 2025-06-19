f07c9ddcdea4e3e0510b8cb87c4ada548aa15e58
dndinidn <andiniputriamrullah@gmail.com>
2025-06-19 20:36:45 +0800

    Update Document

--- update-document.js
+++
@@ -1,5 +0,0 @@
-// Tambah stok produk TV LED 42 inch sebanyak 5
-db.produk.updateOne(
-  { nama: "TV LED 42 inch" },
-  { $inc: { stok: 5 } }
-);
---
+++ update_document.js
@@ -0,0 +1,1 @@
+db.produk.updateOne({ nama: "TV LED 42 inch" }, { $inc: { stok: 4 } });
